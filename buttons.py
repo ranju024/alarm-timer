31064dc951442224957baf6ec74c2f8d4a722982
ranju <sharmaranju684@gmail.com>
2026-02-28 15:07:53 +0545

    Timer Setup Done

--- buttons.py
+++ buttons.py
@@ -32,9 +32,17 @@ class Buttons:
             (450, 150),
             (200, 500)
         ]
+        self.timer_btn_positions = [
+            (50, 150),
+            (300, 150),
+            (450, 150),
+            (200, 500)
+        ]
 
         self.selected_time = [0, 0, 'am']
+        self.selected_timer = [0, 0, 0]
         self.alarm_btns_state = [False, False, False, False]
+        self.timer_btns_state = [False, False, False, False]
 
         self.text = ""
 
@@ -42,9 +50,13 @@ class Buttons:
         alarm_time_str = f"{self.selected_time[0]:02}:{self.selected_time[1]:02} {self.selected_time[2].upper()}"
         return alarm_time_str
 
-    def reset(self):
-        self.is_alarm_set = False
+    def reset_timer(self):
         self.is_timer_set = False
+        self.timer_btns_state = [False, False, False, False]
+        self.selected_timer = [0, 0, 0]
+
+    def reset_alarm(self):
+        self.is_alarm_set = False
         self.alarm_btns_state = [False, False, False, False]
         self.selected_time = [0, 0, 'am']
 
@@ -63,39 +75,33 @@ class Buttons:
             surface.blit(text_surface, (pos[0]+10, pos[1]+5))  #blit rendered text onto surface(screen)
 
 
-    def draw_alarm_buttons(self, pygame, surface):
-        for index, pos in enumerate(self.alarm_btn_positions):
-            mpos = self.get_mouse_positions()
-            if index == 3:
-                w, h = 200, 80
-                font = self.fontl
-                text = 'Activate'
-            else: 
-                w, h = self.btn_w, self.btn_h
-                font = self.btn_font
-                if index == 0: 
-                    text = 'Hour'
-                elif index == 1:
-                    text = 'Minutes'
-                elif index == 2:
-                    text = 'AM/PM'
-            pygame.draw.rect(surface, self.rect_color, [pos[0], pos[1], w,h], width=2, border_radius=5)
+    # # def draw_alarm_buttons(self, pygame, surface):
+    #     for index, pos in enumerate(self.alarm_btn_positions):
+    #         mpos = self.get_mouse_positions()
+    #         if index == 3:
+    #             w, h = 200, 80
+    #             font = self.fontl
+    #             text = 'Activate'
+    #         else: 
+    #             w, h = self.btn_w, self.btn_h
+    #             font = self.btn_font
+    #             if index == 0: 
+    #                 text = 'Hour'
+    #             elif index == 1:
+    #                 text = 'Minutes'
+    #             elif index == 2:
+    #                 text = 'AM/PM'
+    #         self.pygame.draw.rect(surface, self.rect_color, [pos[0], pos[1], w,h], width=2, border_radius=5)
 
-            # checking for mouse hover
-            if self.button_hover(pos):
-                pygame.draw.rect(surface, self.rect_color_selected, [pos[0], pos[1], w, h], width=2, border_radius=5)
-                text_surface = font.render(text, True, self.text_color_selected)
-            else:
-                text_surface = font.render(text, True, self.text_color_normal)
+    #         # checking for mouse hover
+    #         if self.button_hover(pos):
+    #             self.pygame.draw.rect(surface, self.rect_color_selected, [pos[0], pos[1], w, h], width=2, border_radius=5)
+    #             text_surface = font.render(text, True, self.text_color_selected)
+    #         else:
+    #             text_surface = font.render(text, True, self.text_color_normal)
 
-            # check if a button was clicked, then set its color
-            # self.selected_alarm_button = self.alarm_button_clicked(mpos[0], mpos[1])
-            # if self.selected_alarm_button == index:
-            #     pygame.draw.rect(surface, self.color_selected, [pos[0], pos[1], w, h], width=2, border_radius=5)
-            #     text_surface = font.render(text, True, self.text_color_selected)
-
-            surface.blit(text_surface, (pos[0] + 13, pos[1]))
-
+    #         surface.blit(text_surface, (pos[0] + 13, pos[1]))
+  
     def get_mouse_positions(self):
         mouse_pos = self.pygame.mouse.get_pos()
         return mouse_pos
@@ -103,80 +109,61 @@ class Buttons:
     def get_mouse_click(self, x, y) -> int|None:
         return self.button_clicked(x, y)
 
-    # def get_alarm_click(self, pygame, screen, mx, my):
-    #     self.selected_alarm_button = self.alarm_button_clicked(mx, my)
-    #     btn = self.selected_alarm_button
-    #     text = ''
-    #     if btn != None:
-    #         done = False
-    #         while not done:
-    #             for event in pygame.event.get():
-    #                 # if event.type == pygame.MOUSEBUTTONDOWN:
-    #                 #     # Toggle active state if the user clicks on the input box
-    #                 #     if input_box.collidepoint(event.pos):
-    #                 #         active = not active
-    #                 #     else:
-    #                 #         active = False
-    #                     # color = color_active if active else color_inactive
-    #                 if event.type == pygame.KEYDOWN:
-    #                     if event.key == pygame.K_RETURN:
-    #                         done = True
-    #                     elif event.key == pygame.K_BACKSPACE:
-    #                         text = text[:-1]
-    #                     # Only append characters that are digits
-    #                     elif event.unicode.isdigit(): 
-    #                         text += event.unicode
-
-    #             # Render the current input text
-    #             prompt_surface = self.fonts.render(str(text), True, (255, 0, 255))
-    #             screen.blit(prompt_surface, self.alarm_btn_positions[btn])
-
-    #             pygame.display.update()
-
-    #         try:
-    #             if btn == 0:
-    #                 hr = int(text)
-    #             elif btn == 1:
-    #                 min = int(text)
-        
-    #             return int(text)     # if '.' not in text else float(text)
-    #         except ValueError:
-    #             return None # Handle cases where input is empty or invalid
-    #     # if btn == 0: ask user to input hours
-    #     # if btn == 1: ask user to input minutes
-    #     # if btn == 2: ask user to input am/pm
-    #     # if btn == 3: activate the alarm (current time, final time, ...)
-
-    def finalize_time_input(self, index):
-        """Finalizes the hour or minute input when Enter is pressed."""
+    def finalize_time_input(self, index, wdow):
+        """Finalizes the hour and minute input (for alarm) and hour, minute and seconds input (for timer) when Enter is pressed."""
         try:
             value = int(self.text)
             if index == 0:  # Hour
                 if 1 <= value <= 12:
-                    self.selected_time[index] = value
+                    if wdow == 0:
+                        self.selected_time[index] = value 
+                    elif wdow == 1:
+                        self.selected_timer[index] = value
             elif index == 1:  # Minutes
                 if 0 <= value < 60:
-                    self.selected_time[index] = value
+                    if wdow == 0:
+                        self.selected_time[index] = value 
+                    elif wdow == 1:
+                        self.selected_timer[index] = value
+            elif index == 2 and wdow == 1:  # seconds for timer
+                if 0 <= value < 60:
+                    self.selected_timer[index] = value
         except ValueError:
             # Handle cases where input is not a valid number
             print(f"{self.selected_time[index]} is not a valid number")
-            self.selected_time[index] = 0
+            if wdow == 0:
+                self.selected_time[index] = 0
+            elif wdow == 1:
+                self.selected_timer[index] = 0
         self.text = "" # Clear input box text after submission
 
 
-    def handle_time_input(self, surface, event, pygame, index):
-        pos = self.alarm_btn_positions[index]
-        """Handles digit input, backspace, and rendering for text fields."""
-        if event.key == pygame.K_BACKSPACE:
-            self.text = self.text[:-1]
-        elif event.unicode.isdigit() and len(self.text) < 2:
-            self.text += event.unicode
+    def handle_time_input(self, surface, event, pygame, index, wdow):
+        if wdow == 0:
+            pos = self.alarm_btn_positions[index]
+            """Handles digit input, backspace, and rendering for text fields."""
+            if event.key == pygame.K_BACKSPACE:
+                self.text = self.text[:-1]
+            elif event.unicode.isdigit() and len(self.text) < 2:
+                self.text += event.unicode
+            
+            # Update the text surface immediately after changing the text
+            txt_surface = self.btn_font.render(str(self.text), True, (255,255,255))
+            # Now blit the text
+            surface.blit(txt_surface, pos)
+            pygame.display.update()
         
-        # Update the text surface immediately after changing the text
-        txt_surface = self.btn_font.render(str(self.text), True, (255,255,255))
-        # Now blit the text
-        surface.blit(txt_surface, pos)
-        pygame.display.update()
+        elif wdow == 1: # timer window
+            pos = self.timer_btn_positions[index]
+            if event.key == pygame.K_BACKSPACE:
+                self.text = self.text[:-1]
+            elif event.unicode.isdigit() and len(self.text) < 2:
+                self.text += event.unicode
+            
+            txt_surface = self.btn_font.render(str(self.text), True, (255,255,255))
+            surface.blit(txt_surface, pos)
+            pygame.display.update()
+
 
     def get_alarm_click(self, pygame, screen):
         for index, pos in enumerate(self.alarm_btn_positions):
@@ -192,39 +179,34 @@ class Buttons:
 
             # Clear the area with a background color
             screen.fill(color, button_rect) 
-
-        # now handle user inputs (hour, min, toggle am-pm) when user presses the button
-        # self.selected_time = [0, 0, 'am']
-        # self.alarm_btns_state = [False, False, False, False]
-            # if self.alarm_btns_state[index] == True:
-            #     if index == 0 or index == 1:
-            #         for event in pygame.event.get():
-            #             if event.type == pygame.KEYDOWN:
-            #                 if event.key == pygame.K_RETURN:  # if user presses 'Enter'
-            #                     if index == 0:  # for hour
-            #                         # self.selected_time[index] = self.text if self.text <=12  else 0
-            #                         self.selected_time[index] = int(self.text) if self.text.isdigit() and int(self.text) <=12  else 0
-            #                     elif index == 1:  # for minutes
-            #                         self.selected_time[index] = int(self.text) if self.text.isdigit() and int(self.text) < 60  else 0
-            #                         # self.selected_time[index] = self.text if self.text <60  else 0
-            #                 elif event.key == pygame.K_BACKSPACE:
-            #                     self.text = self.text[:-1]
-            #                 elif event.unicode.isdigit() and len(self.text) < 2:
-            #                     self.text += event.unicode
-            #                 txt_surface = self.btn_font.render(str(self.text), True, text_color)
-            #                 # Now blit the text
-            #                 screen.blit(txt_surface, pos)
-            #         # pygame.draw.rect(screen, self.color, self.rect, 2)
-            #     elif index == 2:  # for am/pm
-            #         self.toggle_ampm()
-            #         txt_surface = self.btn_font.render(self.selected_time[2].upper(), True, text_color)
-            #         screen.blit(txt_surface, pos)
             
             # if user presses on 'Activate' button, show the activated window
             if index == 3:
                 prompt = 'Activate'
             else:
                 prompt = self.selected_time[index]
+            prompt_surface = self.btn_font.render(str(prompt), True, text_color)
+            screen.blit(prompt_surface, pos)
+    
+    def get_timer_click(self, pygame, screen):
+        for index, pos in enumerate(self.timer_btn_positions):
+            # before blitting the text, define the rect for the button
+            button_rect = pygame.Rect(pos[0], pos[1], self.btn_w, self.btn_h) 
+            color = self.btn_color
+            text_color = (255,255,255)
+
+            if self.button_hover(pos) or self.timer_btns_state[index] == True:
+                color = (0,255,255)
+                text_color = (0,0,0)
+
+            # Clear the area with a background color
+            screen.fill(color, button_rect) 
+            
+            # if user presses on 'Activate' button, show the activated window
+            if index == 3:
+                prompt = 'Set Timer'
+            else:
+                prompt = self.selected_timer[index]
             prompt_surface = self.btn_font.render(str(prompt), True, text_color)
             screen.blit(prompt_surface, pos)
 
@@ -243,7 +225,7 @@ class Buttons:
                 return index
         return None
 
-        # set btn state tp True when any button pressed
+    # set btn state to True when any button pressed
     def alarm_button_clicked(self, mouse_x, mouse_y):
         for index, pos in enumerate(self.alarm_btn_positions):
             if self.on_button(mouse_x, mouse_y, pos):
@@ -253,6 +235,16 @@ class Buttons:
                 return index
             elif index != 3:
                 self.alarm_btns_state[index] = False
+
+    def timer_button_clicked(self, mouse_x, mouse_y):
+        for index, pos in enumerate(self.timer_btn_positions):
+            if self.on_button(mouse_x, mouse_y, pos):
+                self.timer_btns_state[index] = True
+                if index == 3:
+                    self.is_timer_set = True
+                return index
+            elif index != 3:
+                self.timer_btns_state[index] = False
 
     # is button currently being hovered?
     def button_hover(self, pos):
--- home.py
+++ home.py
@@ -16,6 +16,8 @@ sound_file = "alarm_sound.mp3"
 pygame.mixer.music.load(sound_file)
 alarm_triggered = False
 alarm_already_playing = False
+timer_already_playing = False
+
 
 ''' Fonts '''
 pygame.font.init()
@@ -28,6 +30,7 @@ bg_color = (0,0,0)
 alarm_window_color = (255, 0, 0)
 timer_window_color = (0, 0, 255)
 
+clock = pygame.time.Clock()
 buttons = Buttons(pygame, font2, font0)
 
 running = True
@@ -38,18 +41,20 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        # if event.type == pygame.KEYDOWN:
-        #     # Reset alarm if user presses 'r' / 'R'
-        #     if event.key == pygame.K_r:  
-                #reset
-
         # stop playing alarm audio when user presses 's' or 'S'
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_s and alarm_already_playing:
-                pygame.mixer.music.stop()
-                alarm_already_playing = False
-                buttons.reset()
-                wdow = None
+            if event.key == pygame.K_s:
+                if alarm_already_playing:
+                    pygame.mixer.music.stop()
+                    alarm_already_playing = False
+                    buttons.reset_alarm()
+                    wdow = None
+                elif timer_already_playing:
+                    pygame.mixer.music.stop()
+                    timer_already_playing = False
+                    buttons.reset_timer()
+                    wdow = None
+
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             if pygame.mouse.get_pressed()[0]:  # if left mouse button clicked
@@ -58,6 +63,7 @@ while running:
                 # only check if previously any of the buttons haven't been pressed!!
                 if wdow == None:
                     wdow = buttons.get_mouse_click(pos[0], pos[1])
+
                 # alarm window not yet activated
                 elif wdow == 0 and buttons.is_alarm_set == False:
                     index = buttons.alarm_button_clicked(pos[0], pos[1])
@@ -66,32 +72,46 @@ while running:
                     if index == 2:  # for am/pm
                         buttons.toggle_ampm()
                         txt_surface = buttons.btn_font.render(buttons.selected_time[2].upper(), True, (255,255,255))
-                        surface.blit(txt_surface, pos)
+                # timer window 
+                elif wdow == 1: 
+                    if buttons.is_timer_set == False:  # not yet activated
+                        index = buttons.timer_button_clicked(pos[0], pos[1])
+                        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_IBEAM)
+                        start_time = pygame.time.get_ticks()
+                
 
-        # handling any keyboard events while user tries to activate the alarm
-        # handle user inputs (hour, min, toggle am-pm) when user presses respective button
+        # handling any keyboard events while user tries to activate the alarm or set the timer
+        # handle user inputs (hour, min, toggle am-pm/seconds) when user presses respective button
         if event.type == pygame.KEYDOWN and index is not None:
-            if index in (0, 1):
-                if event.key == pygame.K_RETURN:  # if user presses 'Enter'
-                    buttons.finalize_time_input(index)
-                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
-                    
-                else:
-                    buttons.handle_time_input(surface, event, pygame, index)
+            if wdow == 0:
+                if index in (0, 1):
+                    if event.key == pygame.K_RETURN:  # if user presses 'Enter'
+                        buttons.finalize_time_input(index, wdow)
+                        
+                    else:
+                        buttons.handle_time_input(surface, event, pygame, index, wdow)
+            if wdow == 1:
+                if index in (0, 1, 2):
+                    if event.key == pygame.K_RETURN:  # if user presses 'Enter'
+                        buttons.finalize_time_input(index, wdow)
+                        
+                    else:
+                        buttons.handle_time_input(surface, event, pygame, index, wdow)
 
+
+    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
                
-             # timer button left
-       
-
     if wdow != None:
-        if wdow == 0: # alarm window
+        
+        if wdow == 0: 
+            '''  alarm window '''
             pygame.display.set_caption('My-Alarm-Window')
             surface.fill(alarm_window_color)
 
             # if alarm hasn't been set, draw buttons with them in active state
             if buttons.is_alarm_set == False:
                 buttons.get_alarm_click(pygame, surface)
-            # if alarm has been set, display timer or maybe the day and time for which it has been set
+            # if alarm has been set, display time for which it has been set
             else:
                 msg = "Your alarm has been set on"
                 alarm_msg = "Time to work!"
@@ -110,7 +130,7 @@ while running:
                         alarm_already_playing = True
                     surface.blit(alarm_msg_surface, (200, 280))
 
-                # Check if the music has stopped (e.g. if the user pressed 's' or it ended naturally)
+                # Check if the music has stopped (if the user pressed 's' or it ended naturally)
                 elif alarm_triggered and not pygame.mixer.music.get_busy():
                     alarm_triggered = False
                 else:
@@ -121,10 +141,47 @@ while running:
                     surface.fill((255,255,255), button_rect)
                     surface.blit(msg_surface1, (50, 240))
                     surface.blit(msg_surface2, (rect_pos[0]+10, 310))
-            # buttons.draw_alarm_buttons( pygame, surface)
+        
         elif wdow == 1:
+            ''' timer window '''
             pygame.display.set_caption('My-Timer-Window')
             surface.fill(timer_window_color)
+
+            # if timer hasn't been set, draw buttons with them in active state
+            if buttons.is_timer_set == False:
+                buttons.get_timer_click(pygame, surface)
+            # if timer has been set, display timer 
+            else:
+                hours = buttons.selected_timer[0]
+                minutes = buttons.selected_timer[1]
+                seconds = buttons.selected_timer[2]            
+                
+                total_seconds = hours*60*60 + minutes*60 + seconds  # converted into seconds
+
+                current_time = pygame.time.get_ticks()
+                elapsed_time = (current_time - start_time) // 1000   # Convert milliseconds to seconds
+
+                if total_seconds - elapsed_time > 0:
+                    time_left = total_seconds - elapsed_time
+                    # Convert time_left back to H:M:S format
+                    minutes, seconds = divmod(time_left, 60)
+                    hours, minutes = divmod(minutes, 60)
+                    timer_text = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+                    
+                else:
+                    timer_text = "Time Up!"
+                    if not timer_already_playing:
+                        pygame.mixer.music.play(-1)   # Play the music on a loop (-1 means infinite loop)
+                        timer_already_playing = True
+                
+                text_surface = font0.render(timer_text, True, (255, 255, 255))
+                # Center the text
+                text_rect = text_surface.get_rect(center=(300, 300))
+                surface.blit(text_surface, text_rect)
+               
+                # Cap the frame rate
+                clock.tick(60) # limits FPS to 60
+
     else:
         surface.fill(bg_color)
         buttons.create_buttons(surface, font2)
